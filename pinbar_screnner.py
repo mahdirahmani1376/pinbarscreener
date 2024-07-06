bd86792c73109395c6be8c0650a640b6374fa22e
ma.rahmani <ma.rahmani@hostiran.com>
2024-07-06 17:50:19 +0330

    add files

--- pinbar_screnner.py
+++ pinbar_screnner.py
@@ -1,30 +1,73 @@
-import requests
-import pandas as pd
+import yfinance as yf
 import matplotlib.pyplot as plt
 from io import BytesIO
-from datetime import datetime
-from telegram import Bot
+import plotly.graph_objects as go
+import requests
+from dotenv import dotenv_values
 
-# Alpha Vantage API Configuration
-ALPHA_VANTAGE_API_KEY = 'SVYMAQZMSDLD794S'
-SYMBOL = 'EUR/USD'
-INTERVAL = '5min'
+# Yahoo Finance Configuration
+symbols = [
+    'EURUSD=X',
+    'JPY=X',
+    'GBPUSD=X'
+]
 
+config = dotenv_values(".env")
 # Telegram Bot Configuration
-TELEGRAM_BOT_TOKEN = 'your_telegram_bot_token'
-TELEGRAM_CHANNEL_ID = '@your_telegram_channel_id'
+interval = config.get('INTERVAL')
+bot_token = config.get('TELEGRAM_BOT_TOKEN')
+channel_id = config.get('TELEGRAM_CHANNEL_ID')
 
 
 # Function to fetch EUR/USD candlestick data
-def fetch_candlestick_data(symbol, interval, api_key):
-    url = f'https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol={symbol.split("/")[0]}&to_symbol={symbol.split("/")[1]}&interval={interval}&apikey={api_key}'
-    response = requests.get(url)
-    data = response.json()
-    df = pd.DataFrame(data['Time Series FX (' + interval + ')']).T
-    df.columns = ['open', 'high', 'low', 'close']
-    df.index = pd.to_datetime(df.index)
-    df = df.astype(float)
+def fetch_candlestick_data(symbol, interval):
+    df = yf.download(tickers=symbol, interval=interval, period='1d')
     return df
+
+
+def show_candle_stick_chart(dataframe):
+    candlestick = go.Candlestick(
+        x=dataframe.index,
+        open=dataframe['Open'],
+        high=dataframe['High'],
+        low=dataframe['Low'],
+        close=dataframe['Close']
+    )
+
+    fig = go.Figure(data=[candlestick])
+    fig.update_layout(xaxis_rangeslider_visible=False, template="plotly_dark")
+
+    fig.show()
+
+
+def save_candle_stick_chart(dataframe, path):
+    candlestick = go.Candlestick(
+        x=dataframe.index,
+        open=dataframe['Open'],
+        high=dataframe['High'],
+        low=dataframe['Low'],
+        close=dataframe['Close']
+    )
+
+    fig = go.Figure(data=[candlestick])
+    fig.update_layout(xaxis_rangeslider_visible=False, template="plotly_dark")
+
+    # fig.write_image(f"charts/{name}.png")
+    fig.write_image(path, width=1920, height=1080)
+
+
+# Function to create a candlestick chart using Plotly
+def create_candlestick_chart(df):
+    candlestick = go.Candlestick(
+        x=df.index,
+        open=df['Open'],
+        high=df['High'],
+        low=df['Low'],
+        close=df['Close']
+    )
+    fig = go.Figure(data=[candlestick])
+    fig.update_layout(xaxis_rangeslider_visible=False, template="plotly_dark")
+    return fig
 
 
 # Function to detect pinbar candles
@@ -36,38 +79,33 @@ def is_pinbar(candle):
     return body <= 0.3 * (high - low) and (upper_wick >= 2 * body or lower_wick >= 2 * body)
 
 
-# Function to send message with candlestick chart to Telegram
-def send_telegram_message(bot_token, channel_id, message, image):
-    bot = Bot(token=bot_token)
-    bot.send_message(chat_id=channel_id, text=message)
-    bot.send_photo(chat_id=channel_id, photo=image)
+def send_telegram_message(bot_token, channel_id, message, fig):
+    requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={channel_id}&text={message}")
+    # Save the figure as an image
+    buf = BytesIO()
+    fig.write_image(buf, format='png', width=1920, height=1080)
+    buf.seek(0)
+    files = {'photo': buf}
+    requests.post(f"https://api.telegram.org/bot{bot_token}/sendPhoto?chat_id={channel_id}",
+                  files=files)
 
 
 # Main function
 def main():
-    # Fetch candlestick data
-    df = fetch_candlestick_data(SYMBOL, INTERVAL, ALPHA_VANTAGE_API_KEY)
+    for symbol in symbols:
+        # Fetch candlestick data
+        df = fetch_candlestick_data(symbol, interval)
 
-    # Check for pinbar in the latest candle
-    latest_candle = df.iloc[-1]
-    if is_pinbar(latest_candle):
-        # Plot the candlestick chart
-        plt.figure(figsize=(10, 5))
-        plt.plot(df.index, df['close'], label='Close Price')
-        plt.fill_between(df.index, df['low'], df['high'], alpha=0.1)
-        plt.title(f'{SYMBOL} 5min Candlestick Chart')
-        plt.xlabel('Time')
-        plt.ylabel('Price')
-        plt.legend()
+        # Check for pinbar in the latest candle
+        latest_candle = df.iloc[-1]
+        # if is_pinbar(latest_candle):
+        if True:
+            # Create the candlestick chart
+            fig = create_candlestick_chart(df)
 
-        # Save plot to image
-        buf = BytesIO()
-        plt.savefig(buf, format='png')
-        buf.seek(0)
-
-        # Send message to Telegram
-        message = f'Pinbar detected on {SYMBOL} 5min timeframe at {df.index[-1]}'
-        send_telegram_message(TELEGRAM_BOT_TOKEN, TELEGRAM_CHANNEL_ID, message, buf)
+            # Send message to Telegram
+            message = f'Pinbar detected on {symbol} 5min timeframe at {df.index[-1]}'
+            send_telegram_message(bot_token, channel_id, message, fig)
 
 
 # Run the main function
